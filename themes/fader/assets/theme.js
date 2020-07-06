90acaee2dc3749a5b0e40c3963ba57fe73e3a38d
Nicholas Young <nicholas@nicholaswyoung.com>
2020-07-06 11:20:56 -0600

    feature(design): launch dark mode toggle

--- themes/fader/assets/theme.js
+++ themes/fader/assets/theme.js
@@ -1,13 +1,27 @@
+function setColorMode(mode) {
+  document.documentElement.setAttribute('color-scheme', mode);
+  localStorage.setItem('color-scheme', mode);
+}
+
 function toggleThemeMode(e) {
-  if (e.currentTarget.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('color-mode', 'dark');
+  if (e.currentTarget.className.indexOf('light') !== -1) {
+    setColorMode('light');
     return;
   }
 
-  document.documentElement.setAttribute('data-theme', 'light');
-  localStorage.setItem('color-mode', 'light');
+  setColorMode('dark');
 }
+
+function detectUserColorMode() {
+  if (localStorage.getItem('color-scheme') === 'dark' ||
+    (window.matchMedia('(prefers-color-scheme: dark)').matches &&
+      !localStorage.getItem('color-scheme')))
+  {
+    document.documentElement.setAttribute('color-scheme', 'dark');
+  }
+}
+
+detectUserColorMode();
 
 const buttons = document.querySelectorAll('.theme-toggle');
 buttons.forEach(button => {
