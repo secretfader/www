69d0f99fc51f4fbeb2fb702c59d12a81120fa435
Nicholas Young <hi@secretfader.com>
2021-04-10 10:29:16 -0600

    chore: update install-hugo.sh to only use released versions

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-# Retrieve the latest release from Hugo GitHub's API
-HUGO_URL=$(curl -sS https://api.github.com/repos/gohugoio/hugo/releases | jq -r ".[0].assets[] | select(.name | test(\"extended\")) | select(.name | test(\"Linux-64bit.tar.gz\")) | .browser_download_url")
+# Retrieve the latest stable, extended release version from GitHub
+DOWNLOAD_URL=$(curl --silent -H 'Accept: application/json' \
+    https://api.github.com/repos/gohugoio/hugo/releases | \
+    jq -r "(.[0] |
+    (select(.draft == false) | select(.prerelease == false)).assets[] |
+        select(.name | contains(\"extended\")) | select(.name | contains(\"Linux-64bit.tar.gz\"))
+    ).browser_download_url"
+)
 
-# Setup local binary directory
+# Make a directory at $HOME/bin to contain downloaded binaries
 mkdir -p $HOME/bin
 
-# Install Hugo
-curl -sSL $HUGO_URL | tar -xzf - -C $HOME/bin
+# Download the Hugo binary and extract the tar.gz archive $HOME/bin
+curl -sSL $DOWNLOAD_URL | tar -xzf - -C $HOME/bin
