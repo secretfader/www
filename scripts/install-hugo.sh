4e4854f1dc1bc071fd8d7ed7e19c4638cf3f83e1
Nicholas Young <hi@secretfader.com>
2021-05-06 08:11:05 -0600

    chore: make installation script more generic

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -3,17 +3,20 @@
 # Terminate early on pipeline failure, since we use pipes below
 set -e
 
-# Retrieve the latest stable, extended release version from GitHub
-DOWNLOAD_URL=$(curl -sS -H 'Accept: application/json' \
-    https://api.github.com/repos/gohugoio/hugo/releases | \
-    jq -r "(.[0] |
-    (select(.draft == false) | select(.prerelease == false)).assets[] |
+HUGO_RELEASES=$(curl -sS -H 'Accept: application/json' \
+    https://api.github.com/repos/gohugoio/hugo/releases
+)
+    
+RELEASE_FILTER="(.[0] |
+   (select(.draft == false) | select(.prerelease == false)).assets[] |
         select(.name | contains(\"extended\")) | select(.name | contains(\"Linux-64bit.tar.gz\"))
     ).browser_download_url"
-)
+
+# Retrieve the latest stable, extended release version of from GitHub
+HUGO_DOWNLOAD_URL=$(jq -r "$RELEASE_FILTER" <<< $HUGO_RELEASES)
 
 # Make a directory at $HOME/bin to contain downloaded binaries
 mkdir -p $HOME/bin
 
 # Download the Hugo binary and extract the tar.gz archive $HOME/bin
-curl -sSL $DOWNLOAD_URL | tar -xzf - -C $HOME/bin
+curl -sSL $HUGO_DOWNLOAD_URL | tar -xzf - -C $HOME/bin
