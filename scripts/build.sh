1c0e0f5df0086e13000a01ff0e1ea6481791ee46
Nicholas Young <hi@secretfader.com>
2021-05-06 18:47:39 -0600

    chore: add npm binaries to $PATH

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env /bin/sh
-$HOME/bin/hugo
+
+# Extend $PATH with $HOME/bin, using the npm binary path as a last option
+PATH=$HOME/bin:$PATH:$(npm bin)
+
+# Create built artifacts
+hugo
