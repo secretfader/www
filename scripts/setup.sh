b77df4b9767f04e686172e6c05b4316a869a290a
Nicholas Young <hi@secretfader.com>
2021-05-07 19:30:19 -0600

    chore: make installation scripts more comprehensive

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,17 +6,22 @@ set -e
 HUGO_RELEASES=$(curl -sS -H 'Accept: application/json' \
     https://api.github.com/repos/gohugoio/hugo/releases
 )
-    
-RELEASE_FILTER="(.[0] |
-   (select(.draft == false) | select(.prerelease == false)).assets[] |
-        select(.name | contains(\"extended\")) | select(.name | contains(\"Linux-64bit.tar.gz\"))
-    ).browser_download_url"
+
+LATEST_VERSION="select(.draft == false) | select(.prerelease == false)
+    .assets[] |
+        select(.name | contains(\"extended\")) |
+        select(.name | contains(\"Linux-64bit.tar.gz\"))"
+
+DOWNLOAD_URL="(.[0] | $LATEST_VERSION).browser_download_url"
 
 # Retrieve the latest stable, extended release version of from GitHub
-HUGO_DOWNLOAD_URL=$(printf "%s" "$HUGO_RELEASES" | jq -r "$RELEASE_FILTER")
+HUGO_DOWNLOAD_URL=$(printf "%s" "$HUGO_RELEASES" | jq -r "$DOWNLOAD_URL")
 
 # Make a directory at $HOME/bin to contain downloaded binaries
 mkdir -p $HOME/bin
 
 # Download the Hugo binary and extract the tar.gz archive $HOME/bin
 curl -sSL $HUGO_DOWNLOAD_URL | tar -xzf - -C $HOME/bin
+
+# Install npm dependencies (in CI mode)
+npm ci
