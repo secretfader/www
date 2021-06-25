adcbf53d12137573e87b48d8a789793d2b5e1d21
Nicholas Young <hi@secretfader.com>
2021-06-25 17:17:03 -0600

    feature: cinstall sd CLI tool via scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,25 +3,37 @@
 # Terminate early on pipeline failure, since we use pipes below
 set -e
 
+LATEST_VERSION="select(.draft == false) | select(.prerelease == false) .assets[]"
+
+LINUX_GNU="select(.name | contains(\"linux-gnu\"))"
+
+LATEST_HUGO="$LATEST_VERSION |
+        select(.name | contains(\"extended\")) |
+        select(.name | contains(\"Linux-64bit.tar.gz\"))"
+
+LATEST_SD="$LATEST_VERSION |
+    select(.name | contains(\"linux-gnu\"))"
+
 HUGO_RELEASES=$(curl -sS -H 'Accept: application/json' \
     https://api.github.com/repos/gohugoio/hugo/releases
 )
 
-LATEST_VERSION="select(.draft == false) | select(.prerelease == false)
-    .assets[] |
-        select(.name | contains(\"extended\")) |
-        select(.name | contains(\"Linux-64bit.tar.gz\"))"
+SD_RELEASES=$(curl -sS -H 'Accept: application/json' \
+    https://api.github.com/repos/chmln/sd/releases
+)
 
-DOWNLOAD_URL="(.[0] | $LATEST_VERSION).browser_download_url"
+# Retrieve the latest stable, extended release version of Hugo from GitHub
+HUGO_DOWNLOAD_URL=$(printf "%s" "$HUGO_RELEASES" | jq -r "(.[0] | $LATEST_HUGO).browser_download_url")
 
-# Retrieve the latest stable, extended release version of from GitHub
-HUGO_DOWNLOAD_URL=$(printf "%s" "$HUGO_RELEASES" | jq -r "$DOWNLOAD_URL")
+# Retrieve the latest stable version of `sd` from GitHub
+SD_DOWNLOAD_URL=$(printf "%s" "$SD_RELEASES" | jq -r "(.[0] | $LATEST_SD).browser_download_url")
 
 # Make a directory at $HOME/bin to contain downloaded binaries
 mkdir -p $HOME/bin
 
 # Download the Hugo binary and extract the tar.gz archive $HOME/bin
 curl -sSL $HUGO_DOWNLOAD_URL | tar -xzf - -C $HOME/bin
+curl -sSL $SD_DOWNLOAD_URL -o $HOME/bin/sd
 
 # Install npm dependencies (in CI mode)
 npm ci
