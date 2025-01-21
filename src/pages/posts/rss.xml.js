93ea64ea21880ecc03e6ae2286d2e9d68721cb17
Nicholas Young <hi@secretfader.com>
2025-01-21 11:53:39 -0700

    add initial layout for posts

---
+++ src/pages/posts/rss.xml.js
@@ -0,0 +1,14 @@
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+
+export async function GET(ctx) {
+  const collection = await getCollection("posts");
+
+  return rss({
+    title: "Nicholas Young | Posts",
+    description: "Posts from Nicholas Young's Blog",
+    site: ctx.site,
+    items: [],
+    customData: `<language>en-us</language>`,
+  });
+}
