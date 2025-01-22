02ab83f5f5d4b42dd48a73e5f51dbf80acf2f13a
Nicholas Young <hi@secretfader.com>
2025-01-22 11:45:59 -0700

    rework posts generation

--- src/pages/posts.xml.js
+++ src/pages/posts.xml.js
@@ -1,14 +1,20 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 
+const collectionToRSSItems = async (posts) =>
+  posts.map((post) => ({
+    title: post.data.title,
+    pubDate: post.data.pubDate,
+    link: post.slug,
+  }));
+
 export async function GET(ctx) {
   const collection = await getCollection("posts");
-
   return rss({
     title: "Nicholas Young | Posts",
     description: "Posts from Nicholas Young's Blog",
     site: ctx.site,
-    items: collection,
+    items: await collectionToRSSItems(collection),
     customData: `<language>en-us</language>`,
   });
 }
