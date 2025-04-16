c93bdea040c5a96ba99fd8c7b20e49eff28c5bdd
Nicholas Young <hi@secretfader.com>
2025-04-16 11:52:09 -0600

    chore: fix CF_PAGES_URL env var

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+const site = import.meta.env.CF_PAGES_URL || "https://secretfader.com";
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CF_PAGES_URL || "https://secretfader.com",
+  site,
   integrations: [tailwind(), sitemap()],
 });
