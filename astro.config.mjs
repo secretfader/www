409567990ce35abc1fa2cd5f64fe17fcd98b76d4
Nicholas Young <hi@secretfader.com>
2025-04-21 15:55:18 -0600

    Supporting components for email list (#31)
    * chore: setup resend
    
    * chore: add astro cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 const site = import.meta.env.CF_PAGES_URL || "https://secretfader.com";
 
 // https://astro.build/config
 export default defineConfig({
   site,
   integrations: [tailwind(), sitemap()],
+  adapter: cloudflare(),
 });
