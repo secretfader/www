ac8274991e9ecbbcc041c790f230071503df1c3c
Nicholas Young <hi@secretfader.com>
2025-05-14 18:10:45 -0600

    chore: install deps Alpine.js and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
+import alpine from "@astrojs/alpinejs";
 import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@tailwindcss/vite";
 
 const site = import.meta.env.CF_PAGES_URL || "https://secretfader.com";
 
 // https://astro.build/config
 export default defineConfig({
   site,
-  integrations: [tailwind(), sitemap()],
+  vite: { plugins: [tailwind()] },
+  integrations: [sitemap(), alpine()],
   adapter: cloudflare(),
 });
