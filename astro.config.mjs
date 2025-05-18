ef27e784aedc719b24f1ca5cad23cdcee10efda9
Nicholas Young <hi@secretfader.com>
2025-05-17 18:09:52 -0600

    chore: add sitemap and cloudflare adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@tailwindcss/vite";
 
 const site = process.env.CF_PAGES_URL || "https://secretfader.com";
@@ -7,10 +9,12 @@ const site = process.env.CF_PAGES_URL || "https://secretfader.com";
 // https://astro.build/config
 export default defineConfig({
   site,
+  integrations: [sitemap()],
   vite: { plugins: [tailwind()] },
   redirects: {
     "/archives/podcasts/dispatch.xml": "/podcasts/dispatch/feeds/audio.xml",
     "/archives/podcasts/inside-the-machine.xml":
       "/podcasts/inside-the-machine/feeds/audio.xml",
   },
+  adapter: cloudflare(),
 });
