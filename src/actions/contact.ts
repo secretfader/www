e0e9e573f5be0f90dd535eb26c58ba0a9b92903a
Nicholas Young <hi@secretfader.com>
2025-05-22 07:48:59 -0600

    undo the reversion of "fix: contact form"
    This reverts commit 4ca462202bd06a995912ff6e772e9f15487244eb.

--- src/actions/contact.ts
+++ src/actions/contact.ts
@@ -1,14 +1,29 @@
 import { defineAction, ActionError } from "astro:actions";
 import { z } from "astro:schema";
 
+const webhook = import.meta.env.DISCORD_WEBHOOK;
+
 const input = z.object({
   name: z.string(),
   email: z.string(),
   message: z.string(),
 });
 
-const handler = async (input) => {
-  console.log(input);
+const handler = async ({ name, email, message }) => {
+  const response = await fetch(webhook, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: `${name} - ${email} - ${message}`,
+      username: "Secret Fader Contact Form",
+    }),
+  });
+
+  if (!response.ok) {
+    throw new ActionError({ code: "INTERNAL_SERVER_ERROR" });
+  }
 };
 
 export const action = defineAction({
