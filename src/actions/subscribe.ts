8f9e3e7b04d1f112c438abad100a9d23b6dad5a6
Nicholas Young <hi@secretfader.com>
2025-05-28 11:54:44 -0600

    fix

--- src/actions/subscribe.ts
+++ src/actions/subscribe.ts
@@ -2,7 +2,7 @@ import { defineAction, ActionError } from "astro:actions";
 import { z } from "astro:schema";
 import { experimental_AstroContainer } from "astro/container";
 import { Resend } from "resend";
-import lutsTemplate from "../emails/LUTs.astro";
+import LutsTemplate from "../emails/LUTs.astro";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 const audienceId = "b5840c4f-4aa9-4c90-820e-6e283bb342a9";
@@ -15,8 +15,6 @@ const input = z.object({
 });
 
 const handler = async ({ firstName, lastName, email, afterAction }, ctx) => {
-  const container = await experimental_AstroContainer.create();
-
   const response = await resend.contacts.create({
     audienceId,
     firstName,
@@ -32,13 +30,17 @@ const handler = async ({ firstName, lastName, email, afterAction }, ctx) => {
   }
 
   if (afterAction && afterAction === "luts") {
+    const container = await experimental_AstroContainer.create();
+
+    const html = await container.renderToString(LutsTemplate, {
+      props: { firstName },
+    });
+
     const send = await resend.emails.send({
       from: `Nicholas Young <hi@secretfader.com>`,
       to: [email],
       subject: `Download your HDR to SDR LUTs`,
-      html: await container.renderToString(lutsTemplate, {
-        props: { firstName },
-      }),
+      html,
     });
 
     if (send.error) {
